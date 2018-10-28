3b239740628eb73306c92efdfb1108c350b5b5a6
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-10-28 14:26:46 +0100

    [Entities] Add

--- doodLbot/Entities/Hero.cs
+++ doodLbot/Entities/Hero.cs
@@ -7,9 +7,18 @@ namespace doodLbot.Entities
 {
     public class Hero : Entity
     {
+        // Remains list of objects until the classes are made
+        public IReadOnlyList<object> Gear => this.gear.AsReadOnly();
+        public IReadOnlyList<object> Modules => this.modules.AsReadOnly();
+
+        private readonly List<object> gear;
+        private readonly List<object> modules;
+
+
         public Hero() : base()
         {
-
+            this.gear = new List<object>();
+            this.modules = new List<object>();
         }
     }
 }
