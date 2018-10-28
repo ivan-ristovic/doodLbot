1da45340f497a787df5af01823b14692fef6cc6b
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-10-28 16:12:35 +0100

    [Logic] Add native game logic

---
+++ doodLbot/Logic/GameState.cs
@@ -0,0 +1,23 @@
+﻿using doodLbot.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace doodLbot.Logic
+{
+    public class GameState
+    {
+        public Hero Hero { get; }
+        public IReadOnlyList<Enemy> Enemies => this.enemies.AsReadOnly();
+
+        private readonly List<Enemy> enemies;
+
+
+        public GameState(Hero hero, IEnumerable<Enemy> enemies)
+        {
+            this.Hero = hero;
+            this.enemies = enemies.ToList();
+        }
+    }
+}
