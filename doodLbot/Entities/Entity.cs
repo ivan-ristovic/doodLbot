f51033e05d3377a9ae6c0eeca22b1c7be91d768e
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-10-28 14:09:29 +0100

    [Project] Rename project to doodLbot

---
+++ doodLbot/Entities/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace doodLbot.Entities
+{
+    public class Entity
+    {
+        public double Xpos { get; private set; }
+        public double Ypos { get; private set; }
+        public double Xvel { get; private set; }
+        public double Yvel { get; private set; }
+        public double Hp { get; private set; }
+        public double Damage { get; private set; }
+
+
+        public Entity()
+        {
+            this.Hp = 100;
+            this.Damage = 1;
+        }
+    }
+}
