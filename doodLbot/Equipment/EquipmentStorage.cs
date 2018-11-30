aca68e97e5f86287b671175d390d9b71792748c8
kredenac <zacementirano@gmail.com>
2018-11-30 18:08:08 +0100

    Added a bit of logic for buying/selling

--- doodLbot/Equipment/EquipmentStorage.cs
+++ doodLbot/Equipment/EquipmentStorage.cs
@@ -3,7 +3,7 @@ using doodLbot.Logic;
 
 using System.Collections.Generic;
 using Newtonsoft.Json;
-
+using System.Linq;
 
 
 namespace doodLbot.Equipment
@@ -19,14 +19,37 @@ namespace doodLbot.Equipment
         [JsonProperty("items")]
         public List<ShopEntry> Items;
 
-        public void BuyItem(Gear e)
+        public void BuyItem(string name)
         {
-
+            var item = FindItemFromName(name);
+            item.Count++;
+            // TODO add to hero
         }
 
-        public void SellItem(Gear e)
+        public void SellItem(string name)
         {
+            var item = FindItemFromName(name);
+            if (item.Count <= 0)
+            {
+                throw new System.Exception("Cannot sell when item.count is 0");
+            }
+            item.Count--;
+        }
 
+        private ShopEntry FindItemFromName(string name)
+        {
+            var candidate = Items.Where(e => e.Element.Name == name);
+            var count = candidate.Count();
+            if (count == 0)
+            {
+                throw new System.Exception("No such item");
+            }
+            else if (count > 1)
+            {
+                throw new System.Exception("Multiple items with same name");
+            }
+
+            return candidate.First();
         }
 
         public EquipmentStorage()
