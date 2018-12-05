ef5f302f5148b023345cc15c4a9ac09ca282a0fd
Ivan Ristovic <ivo.ristovic@gmail.com>
2018-12-05 15:17:32 +0100

    [Entities] Add new entities

---
+++ doodLbot/Entities/Shooter.cs
@@ -0,0 +1,37 @@
+﻿using doodLbot.Common;
+using doodLbot.Logic;
+
+using Newtonsoft.Json;
+
+using System.Collections.Generic;
+
+namespace doodLbot.Entities
+{
+    /// <summary>
+    /// Represents an Enemy with extra HP that moves towards a hero and tries to collide with him.
+    /// </summary>
+    public class Shooter : Enemy
+    {
+        [JsonProperty("projectiles")]
+        public IReadOnlyCollection<Projectile> Projectiles => this.projectiles;
+
+        private readonly ConcurrentHashSet<Projectile> projectiles = new ConcurrentHashSet<Projectile>();
+
+
+        public Shooter() : base(speed: Design.EnemySpeed, hp: 50, damage: 25)
+        {
+
+        }
+
+
+        public void Fire(double speed, double damage)
+        {
+            this.projectiles.Add(new Projectile(this.Xpos, this.Ypos, this.Rotation, speed, damage));
+        }
+
+        public bool TryRemoveProjectile(Projectile p)
+        {
+            return this.projectiles.TryRemove(p);
+        }
+    }
+}
---
+++ doodLbot/Entities/Tank.cs
@@ -0,0 +1,15 @@
+﻿using doodLbot.Logic;
+
+namespace doodLbot.Entities
+{
+    /// <summary>
+    /// Represents an Enemy with extra HP that moves towards a hero and tries to collide with him.
+    /// </summary>
+    public class Tank : Enemy
+    {
+        public Tank() : base(speed: Design.EnemySpeed, hp: 1000, damage: 5)
+        {
+
+        }
+    }
+}
